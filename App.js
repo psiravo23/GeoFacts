b1ef9333604b626d58f5a99fb9928686d9f5e6e8
Pete Siravo <p.siravojr@gmail.com>
2020-03-22 17:53:41 -0400

    Setup project structure,added timeStamp to bottom of screen

--- App.js
+++ App.js
@@ -1,19 +1,68 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {AsyncStorage} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+
+class VillanovaTourScreen extends React.Component {
+
+    constructor(props) {
+      super(props)
+      var date = new Date().toLocaleString();
+      this.state = {timeStamp: date};
+    }
+
+    render(){
+      return (
+        <View style={styles.timeStampText}>
+          <Text> {this.state.timeStamp} </Text>
+        </View>
+      );
+    }
+
+}
+
+export default class App extends React.Component {
+
+    storeBuildings = async () => {
+      try {
+        const time = await AsyncStorage.getItem("timeStamp");
+        if (time == null){
+          await AsyncStorage.setItem("timeStamp", new Date().toLocaleString());
+          var jsonData = require("./Facts.json");
+        }
+        else {
+          await AsyncStorage.clear();
+        }
+      }
+      catch (error){
+      }
+    }
+
+    componentDidMount(){
+      {this.storeBuildings()}
+    }
+
+    render(){
+      return (
+        <View style={styles.screenContainer}>
+          <VillanovaTourScreen/>
+        </View>
+      );
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  timeStampText: {
+    position: 'absolute',
+    bottom: 0,
+    height: 70,
+    fontSize: 15,
+  }
 });
