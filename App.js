8e3ecf2d958aeb6fcdc2c69fe2200c102c941865
Pete Siravo <p.siravojr@gmail.com>
2020-03-23 14:41:04 -0400

    Figured out how to call function after findGPSCords is finished.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 import {AsyncStorage} from 'react-native';
 
 
@@ -10,13 +10,40 @@ class VillanovaTourScreen extends React.Component {
       super(props)
       var date = new Date().toLocaleString();
       this.state = {timeStamp: date};
+
+      this.findGPSCords = this.findGPSCords.bind(this);
+      this.handlePress = this.handlePress.bind(this);
+    }
+
+    findGPSCords() {
+      navigator.geolocation.getCurrentPosition(
+       (position) => {
+          this.setState({currLong: position.coords.longitude, currLat: position.coords.latitude}, this.handlePress());
+       },
+       (error) => this.setState({error: error.message}),
+       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+     );
+    }
+
+    handlePress(){
+        this.setState({isFinished: "finished"});
     }
 
     render(){
       return (
-        <View style={styles.timeStampText}>
-          <Text> {this.state.timeStamp} </Text>
-        </View>
+          <View>
+            <View style={styles.screenContainer}>
+              <Button
+                  onPress={this.findGPSCords}
+                  title="Tell Me About My Location"
+              />
+              <Text> {this.state.currLong} {this.state.currLat} </Text>
+              <Text> {this.state.isFinished} </Text>
+            </View>
+            <View style={styles.timeStampText}>
+              <Text> {this.state.timeStamp} </Text>
+            </View>
+          </View>
       );
     }
 
@@ -40,7 +67,7 @@ export default class App extends React.Component {
     }
 
     componentDidMount(){
-      {this.storeBuildings()}
+      {this.storeBuildings}
     }
 
     render(){
@@ -64,5 +91,6 @@ const styles = StyleSheet.create({
     bottom: 0,
     height: 70,
     fontSize: 15,
+    alignItems: 'center'
   }
 });
